f5ab66f8542ed968ea43fe6fa4990dba2c28c8ba
GiblerW <westongibler@gmail.com>
2018-10-08 16:57:06 -0600

    Initial Commit

---
+++ chitchat/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  chitchat
+//
+//  Created by Weston Gibler on 10/8/18.
+//  Copyright © 2018 Weston Gibler. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
